2f2190b2f0c213b11ef0730b6bbe03c5e3c9810a
Weston Platter <westonplatter@gmail.com>
2013-09-30 14:54:12 -0600

    copy and replaced controllers from twinge/questionnaire_engine into crugloabl/qe

--- app/controllers/qe/admin/email_templates_controller.rb
+++ app/controllers/qe/admin/email_templates_controller.rb
@@ -1,62 +1,75 @@
-class Admin::EmailTemplatesController < ApplicationController
-  unloadable
-  before_filter :check_valid_user
-  layout 'qe.admin'
-  
-  def index 
-    @email_templates = EmailTemplate.order('name')
-  
-    respond_to do |format|
-      format.html
-    end
-  end
-  
-  def new 
-    @email_template = EmailTemplate.new
-  
-    respond_to do |format|
-      format.html
-    end
-  end
-  
-  def edit
-    @email_template = EmailTemplate.find(params[:id])
-    
-    respond_to do |format|
-      format.html
-    end
-  end
-  
-  def create
-    @email_template = EmailTemplate.new(params[:email_template])
-    
-    respond_to do |format|
-      if @email_template.save
-        format.html { redirect_to admin_email_templates_path }
-      else
-        format.html { render :action => :new }
-      end
-    end
-  end
-  
-  def update 
-    @email_template = EmailTemplate.find(params[:id])
-    
-    respond_to do |format|
-      if @email_template.update_attributes(params[:email_template])
-        format.html { redirect_to admin_email_templates_path }
-      else
-        format.html { render :action => "edit" }
-      end
-    end
-  end
-  
-  def destroy
-    @email_template = EmailTemplate.find(params[:id])
-    @email_template.destroy
+module Qe
+  module Admin
+    class EmailTemplatesController < ::ApplicationController
+      
+      module M
+        extend ActiveSupport::Concern
+        # included do 
+        #   before_fitler :some_filter
+        # end
 
-    respond_to do |format|
-      format.html { redirect_to email_templates_path }
+        def index 
+          @email_templates = Qe::EmailTemplate.order('name')
+        
+          respond_to do |format|
+            format.html
+          end
+        end
+        
+        def new 
+          @email_template = Qe::EmailTemplate.new
+        
+          respond_to do |format|
+            format.html
+          end
+        end
+        
+        def edit
+          @email_template = Qe::EmailTemplate.find(params[:id])
+          
+          respond_to do |format|
+            format.html
+          end
+        end
+        
+        def create
+          # TODO mass-assignment engineering
+          @email_template = Qe::EmailTemplate.new(params[:email_template], :without_protection => true)
+          
+          respond_to do |format|
+            if @email_template.save
+              format.html { redirect_to admin_email_templates_path }
+            else
+              format.html { render :action => :new }
+            end
+          end
+        end
+        
+        def update 
+          # TODO mass-assignment engineering
+          @email_template = Qe::EmailTemplate.find(params[:id])
+          
+          respond_to do |format|
+            if @email_template.update_attributes(params[:email_template], :without_protection => true)
+              format.html { redirect_to admin_email_templates_path }
+            else
+              format.html { render :action => "edit" }
+            end
+          end
+        end
+        
+        def destroy
+          @email_template = Qe::EmailTemplate.find(params[:id])
+          @email_template.destroy
+
+          respond_to do |format|
+            format.html { redirect_to admin_email_templates_path }
+          end
+        end
+    	end
+
+      include Qe::Admin::BaseControllerConfigs
+      include M
     end
   end
 end
