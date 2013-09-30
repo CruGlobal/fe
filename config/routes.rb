3a38d26099f15f79b87715cc8da82488422a3edd
Weston Platter <westonplatter@gmail.com>
2013-09-30 15:26:01 -0600

    copied over routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
-Rails.application.routes.draw do
+Qe::Engine.routes.draw do
 
   namespace :admin do
     resources :email_templates
-    resources :question_sheets do
+    resources :question_sheets do 
       member do
         post :archive
         post :unarchive
@@ -10,40 +10,40 @@ Rails.application.routes.draw do
       end
       resources :pages,                               # pages/
                 :controller => :question_pages do         # question_sheet_pages_path(),
-        collection do
-          post :reorder
-        end
-        member do
-          get :show_panel
-        end
-        resources :elements do
-          collection do
-            post :reorder
-          end
-          member do
-            get :remove_from_grid
-            post :use_existing
-            post :drop
-            post :duplicate
-          end
-        end
+                collection do
+                  post :reorder
+                end
+                member do
+                  get :show_panel
+                end
+      resources :elements do
+                collection do
+                  post :reorder
+                end
+                member do
+                  get :remove_from_grid
+                  post :use_existing
+                  post :drop
+                  post :duplicate
+                end
+              end
       end
     end
   end
 
   # form capture and review
   resources :reference_sheets
-  resources :answer_sheets do
+  
+  resources :answer_sheets, :except => :new do
     member do
       post :send_reference_invite
       post :submit
     end
     resources  :page, :controller => :answer_pages do
-      member do
-        post :save_file
-      end
+                member do
+                  post :save_file
+                end
     end
-  end
-
-  resources :elements
+  end                 
+  
 end
