c2fc7de1f38d88878cb539d9013a0ba893e05d06
Weston Platter <westonplatter@gmail.com>
2013-10-02 22:42:20 -0600

    use the new "self.table_name=" setter, and not the deprecated method

--- lib/qe.rb
+++ lib/qe.rb
@@ -1,6 +1,10 @@
 require "qe/engine"
 
 module Qe
+  # prefix for database tables
+  mattr_accessor :table_name_prefix
+  self.table_name_prefix ||= 'qe_'
+  
   def self.next_label(prefix, labels)
     max = labels.inject(0) do |m, label|
       num = label[/^#{prefix} ([0-9]+)$/i, 1].to_i   # extract your digits
