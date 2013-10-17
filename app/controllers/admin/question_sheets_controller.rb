4421d63824f2f07fa9050c1971a3bae97844cfd3
Josh Starcher <josh.starcher@gmail.com>
2013-10-17 11:25:47 -0400

    White space cleanup
    Also resolve deprecation notice on find(:all)

--- app/controllers/admin/question_sheets_controller.rb
+++ app/controllers/admin/question_sheets_controller.rb
@@ -5,7 +5,7 @@ class Admin::QuestionSheetsController < ApplicationController
   before_filter :check_valid_user
   before_filter :get_question_sheet, :only => [:show, :archive, :unarchive, :destroy, :edit, :update, :duplicate]
   layout 'qe.admin'
- 
+
   # list of all questionnaires/forms to edit
   # GET /question_sheets
   def index
@@ -17,17 +17,17 @@ class Admin::QuestionSheetsController < ApplicationController
       format.xml  { render :xml => @question_sheets.to_xml }
     end
   end
-  
+
   def archive
     @question_sheet.update_attribute(:archived, true)
     redirect_to :back
   end
-  
+
   def unarchive
     @question_sheet.update_attribute(:archived, false)
     redirect_to :back
   end
-  
+
   def duplicate
     @question_sheet.duplicate
     redirect_to :back
@@ -36,7 +36,7 @@ class Admin::QuestionSheetsController < ApplicationController
   # entry point: display form designer with page 1 and panels loaded
   # GET /question_sheets/1
   def show
-    @all_pages = @question_sheet.pages.find(:all)
+    @all_pages = @question_sheet.pages
     @page = @all_pages[0]
 
     respond_to do |format|
@@ -49,7 +49,7 @@ class Admin::QuestionSheetsController < ApplicationController
   # POST /question_sheets
   def create
     @question_sheet = QuestionSheet.new_with_page
-    
+
     respond_to do |format|
       if @question_sheet.save
         format.html { redirect_to admin_question_sheet_path(@question_sheet) }
@@ -65,12 +65,12 @@ class Admin::QuestionSheetsController < ApplicationController
   # display sheet properties panel
   # GET /question_sheets/1/edit
   def edit
-    
+
     respond_to do |format|
       format.js
     end
   end
-  
+
   # save changes to properties panel (label, language)
   # PUT /question_sheets/1
   def update
@@ -78,7 +78,7 @@ class Admin::QuestionSheetsController < ApplicationController
     respond_to do |format|
       if @question_sheet.update_attributes(params[:question_sheet])
         format.html { redirect_to admin_question_sheet_path(@question_sheet) }
-        format.js 
+        format.js
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -92,13 +92,13 @@ class Admin::QuestionSheetsController < ApplicationController
   # DELETE /question_sheets/1
   def destroy
     @question_sheet.destroy
-    
+
     respond_to do |format|
       format.html { redirect_to admin_question_sheets_path }
       format.xml  { head :ok }
     end
   end
-  
+
   protected
   def get_question_sheet
     @question_sheet = QuestionSheet.find(params[:id])
