71c1afd7e25e6654b7d2510676ff13f08964a7af
Josh Starcher <josh.starcher@gmail.com>
2014-01-23 07:28:54 -0500

    Add :content to permitted params

--- app/controllers/admin/elements_controller.rb
+++ app/controllers/admin/elements_controller.rb
@@ -1,9 +1,9 @@
 class Admin::ElementsController < ApplicationController
   before_filter :check_valid_user
   layout 'qe.admin'
-  
+
   before_filter :get_page
-  
+
   # GET /elements/1
   def show
     @element = Element.find(params[:id])
@@ -16,18 +16,18 @@ class Admin::ElementsController < ApplicationController
   # GET /element/1/edit
   def edit
     @element = Element.find(params[:id])
-    
+
     # for dependencies
     if @element.question?
       (3 - @element.conditions.length).times { @element.conditions.build }
-      @questions_before_this = @page.questions_before_position(@element.position(@page)) 
+      @questions_before_this = @page.questions_before_position(@element.position(@page))
     end
-    
+
     respond_to do |format|
       format.js
     end
   end
-  
+
   def new
     @questions = params[:element_type].constantize.active.order('label')
 
@@ -36,7 +36,7 @@ class Admin::ElementsController < ApplicationController
       @questions = @questions.where(:style => @style).uniq
     end
   end
-  
+
   def use_existing
     @element = Element.find(params[:id])
     # Don't put the same question on a questionnaire twice
@@ -83,7 +83,7 @@ class Admin::ElementsController < ApplicationController
     # Start by removing the element from the page
     page_element = PageElement.where(:element_id => @element.id, :page_id => @page.id).first
     page_element.destroy if page_element
-    
+
     # If this element is not on any other pages, is not a question or has no answers, Destroy it
     if @element.reuseable? && (PageElement.where(:element_id => params[:id]).present? || @element.has_response?)
       @element.update_attributes(:question_grid_id => nil, :conditional_id => nil)
@@ -95,24 +95,24 @@ class Admin::ElementsController < ApplicationController
       format.js
     end
   end
-  
-  def reorder 
+
+  def reorder
     # since we don't know the name of the list, just find the first param that is an array
-    params.each_key do |key| 
+    params.each_key do |key|
       if key.include?('questions_list')
         grid_id = key.sub('questions_list_', '').to_i
         # See if we're ordering inside of a grid
         if grid_id > 0
           Element.find(grid_id).elements.each do |element|
             if index = params[key].index(element.id.to_s)
-              element.position = index + 1 
+              element.position = index + 1
               element.save(:validate => false)
             end
           end
         else
           @page.page_elements.each do |page_element|
             if index = params[key].index(page_element.element_id.to_s)
-              page_element.position = index + 1 
+              page_element.position = index + 1
               page_element.save(:validate => false)
               @element = page_element.element
             end
@@ -120,12 +120,12 @@ class Admin::ElementsController < ApplicationController
         end
       end
     end
-    
+
     respond_to do |format|
       format.js
     end
   end
-  
+
   def drop
     element = Element.find(params[:draggable_element].split('_')[1])  # element being dropped
     target = Element.find(params[:id])
@@ -150,36 +150,36 @@ class Admin::ElementsController < ApplicationController
     # Remove page element for this page since it's now in a grid
     PageElement.where(:page_id => @page.id, :element_id => element.id).first.try(:destroy)
   end
-  
+
   def remove_from_grid
     element = Element.find(params[:id])
     PageElement.create(:element_id => element.id, :page_id => @page.id) unless PageElement.where(:element_id => element.id, :page_id => @page.id).first
     if element.question_grid_id
-      element.set_position(element.question_grid.position(@page), @page) 
+      element.set_position(element.question_grid.position(@page), @page)
       element.question_grid_id = nil
     elsif element.conditional_id
-      element.set_position(element.choice_field.position(@page), @page) 
+      element.set_position(element.choice_field.position(@page), @page)
       element.conditional_id = nil
     end
     element.save!
     render :action => :drop
   end
-  
+
   def duplicate
     element = Element.find(params[:id])
     @element = element.duplicate(@page, element.question_grid || element.choice_field)
     respond_to do |format|
-      format.js 
+      format.js
     end
   end
-  
+
   private
   def get_page
     @page = Page.find(params[:page_id])
   end
 
   def element_params
-    params.fetch(:element, {}).permit(:style, :label, :tooltip, :position, :source, :value_xpath, :text_xpath, :question_grid_id, :cols, :total_cols, :css_id, :css_class, :related_question_sheet_id, :conditional_id, :hide_option_labels, :slug, :required, :is_confidential, :hide_label, :object_name, :attribute_name, :max_length)
+    params.fetch(:element, {}).permit(:content, :style, :label, :tooltip, :position, :source, :value_xpath, :text_xpath, :question_grid_id, :cols, :total_cols, :css_id, :css_class, :related_question_sheet_id, :conditional_id, :hide_option_labels, :slug, :required, :is_confidential, :hide_label, :object_name, :attribute_name, :max_length)
   end
 
 end
