5922434f94ce740e7bbcd1e3f2b3e94d76f9904a
Andrew Roth <andrewroth@gmail.com>
2014-06-25 12:07:33 -0400

    move admin to fe

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,32 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    resources :email_templates
-    resources :question_sheets do
-      member do
-        post :archive
-        post :unarchive
-        post :duplicate
-      end
-      resources :pages,                               # pages/
-                :controller => :question_pages do         # question_sheet_pages_path(),
-        collection do
-          post :reorder
-        end
+  namespace "fe" do
+    namespace :admin do
+      resources :email_templates
+      resources :question_sheets do
         member do
-          get :show_panel
+          post :archive
+          post :unarchive
+          post :duplicate
         end
-        resources :elements do
+        resources :pages,                               # pages/
+                  :controller => :question_pages do         # question_sheet_pages_path(),
           collection do
             post :reorder
           end
           member do
-            get :remove_from_grid
-            post :use_existing
-            post :drop
-            post :duplicate
+            get :show_panel
+          end
+          resources :elements do
+            collection do
+              post :reorder
+            end
+            member do
+              get :remove_from_grid
+              post :use_existing
+              post :drop
+              post :duplicate
+            end
           end
         end
       end
