c7616c06518673d4c06a499f5bd1bc4f0ad9fe4f
Josh Starcher <josh.starcher@gmail.com>
2014-08-03 13:40:14 -0400

    FE shouldn't know anything about GlobalRegistry

--- app/models/fe/email_address.rb
+++ app/models/fe/email_address.rb
@@ -1,33 +1,5 @@
-require 'global_registry_methods'
 class Fe::EmailAddress < ActiveRecord::Base
-  include GlobalRegistryMethods
-  include Sidekiq::Worker
   belongs_to :person
 
   self.table_name = "email_addresses"
-
-  def async_push_to_global_registry(parent_id = nil, parent_type = 'person')
-    return unless person
-    
-    person.async_push_to_global_registry unless person.global_registry_id.present?
-    parent_id = person.global_registry_id unless parent_id
-
-    super(parent_id, parent_type)
-  end
-
-  def self.columns_to_push
-    super
-    @columns_to_push + [{ name: 'email', type: 'email' }]
-  end
-
-  def self.push_structure_to_global_registry
-    parent_id = GlobalRegistry::EntityType.get(
-        {'filters[name]' => 'person'}
-    )['entity_types'].first['id']
-    super(parent_id)
-  end
-
-  def self.skip_fields_for_gr
-    %w[id email created_at updated_at global_registry_id person_id]
-  end
 end
--- app/models/fe/phone_number.rb
+++ app/models/fe/phone_number.rb
@@ -1,25 +1,5 @@
-require 'global_registry_methods'
 class Fe::PhoneNumber < ActiveRecord::Base
-  include GlobalRegistryMethods
-  include Sidekiq::Worker
   belongs_to :person
 
   self.table_name = "phone_numbers"
-  def async_push_to_global_registry(parent_id = nil, parent_type = 'person')
-    person.async_push_to_global_registry unless person.global_registry_id.present?
-    parent_id = person.global_registry_id unless parent_id
-
-    super(parent_id, parent_type)
-  end
-
-  def self.push_structure_to_global_registry
-    parent_id = GlobalRegistry::EntityType.get(
-        {'filters[name]' => 'person'}
-    )['entity_types'].first['id']
-    super(parent_id)
-  end
-
-  def self.skip_fields_for_gr
-    %w[id txt_to_email carrier_id email_updated_at created_at updated_at global_registry_id person_id]
-  end
 end
