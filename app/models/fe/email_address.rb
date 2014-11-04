f1778bdd2ac6a0a928334036d8420aa1cfa218b0
Josh Starcher <josh.starcher@gmail.com>
2014-11-03 22:19:40 -0500

    Add email validation

--- app/models/fe/email_address.rb
+++ app/models/fe/email_address.rb
@@ -1,5 +1,6 @@
 class Fe::EmailAddress < ActiveRecord::Base
   belongs_to :person
+  validates :email, :email_format => { :message => "doesn't look right." }
 
   self.table_name = "email_addresses"
 end
