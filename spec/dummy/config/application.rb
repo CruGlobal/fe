bdbe452594ad8ade71ac97100af1685b5ada188d
Andrew Roth <andrewroth@gmail.com>
2015-09-30 15:19:54 -0400

    add locale to reference sheets
    make tests pass again

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -23,7 +23,8 @@ module Dummy
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
+    config.action_view.raise_on_missing_translations = false
   end
 end
 
