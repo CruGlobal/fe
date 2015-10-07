dd656ab791defb9759d9ae07023d1ce0bb97fbec
Andrew Roth <andrewroth@gmail.com>
2015-10-07 14:44:19 -0400

    add generated data so ref sheets are valid

--- spec/factories/reference_sheets.rb
+++ spec/factories/reference_sheets.rb
@@ -1,4 +1,9 @@
 FactoryGirl.define do 
   factory :reference_sheet, class: Fe::ReferenceSheet do
+    sequence(:email) { |n| "email_#{n}@email.com" }
+    sequence(:first_name) { |n| "fn_#{n}" }
+    sequence(:last_name) { |n| "ln_#{n}" }
+    sequence(:phone) { |n| "phone_#{n}" }
+    sequence(:relationship) { |n| "relationship_#{n}" }
   end
 end
