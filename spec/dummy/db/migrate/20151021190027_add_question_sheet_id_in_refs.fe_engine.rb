a8d0f3c085cbf7e518226de97ae17f52af5b2a4b
Andrew Roth <andrewroth@gmail.com>
2015-10-21 22:56:33 -0400

    fix migration setting intial question_sheet_id

--- spec/dummy/db/migrate/20151021190027_add_question_sheet_id_in_refs.fe_engine.rb
+++ spec/dummy/db/migrate/20151021190027_add_question_sheet_id_in_refs.fe_engine.rb
@@ -2,9 +2,8 @@
 class AddQuestionSheetIdInRefs < ActiveRecord::Migration
   def change
     add_column Fe::ReferenceSheet.table_name, :question_sheet_id, :integer
-    Fe::ReferenceSheet.reset_column_information
 
-    # set question_sheet_id on all refs
-    Fe::ReferenceSheet.joins(:question).update_all("question_sheet_id = related_question_sheet_id")
+    # set initial question_sheet_id on all refs
+    Fe::ReferenceSheet.joins(:question).update_all("#{Fe::ReferenceSheet.table_name}.question_sheet_id = #{Fe::ReferenceQuestion.table_name}.related_question_sheet_id")
   end
 end
