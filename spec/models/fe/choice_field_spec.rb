74ae732105f5103278171f3a7ae51a6f37906507
Andrew Roth <andrewroth@gmail.com>
2015-10-27 16:26:13 -0400

    add test for whispace not affecting conditional answers

--- spec/models/fe/choice_field_spec.rb
+++ spec/models/fe/choice_field_spec.rb
@@ -58,9 +58,9 @@ describe Fe::ChoiceField do
       e = create(:choice_field_element, conditional_answer: 'a;b', style: 'drop-down')
       qs.pages << create(:page)
       qs.pages.reload.first.elements << e
-      a = create(:answer, question_id: e, value: 'a', answer_sheet_id: app.id, question_id: e.id)
-      a = create(:answer, question_id: e, value: 'b', answer_sheet_id: app.id, question_id: e.id)
-      a = create(:answer, question_id: e, value: 'c', answer_sheet_id: app.id, question_id: e.id)
+      a = create(:answer, question: e, value: 'a', answer_sheet_id: app.id)
+      a = create(:answer, question: e, value: 'b', answer_sheet_id: app.id)
+      a = create(:answer, question: e, value: 'c', answer_sheet_id: app.id)
       expect(e.conditional_match(app)).to be true
     end
     it 'returns false if any of none of the options match conditional_answer' do
@@ -70,8 +70,8 @@ describe Fe::ChoiceField do
       e = create(:choice_field_element, conditional_answer: 'a;b', style: 'drop-down')
       qs.pages << create(:page)
       qs.pages.reload.first.elements << e
-      a = create(:answer, question_id: e, value: 'c', answer_sheet_id: app.id, question_id: e.id)
-      a = create(:answer, question_id: e, value: 'd', answer_sheet_id: app.id, question_id: e.id)
+      a = create(:answer, question: e, value: 'c', answer_sheet_id: app.id)
+      a = create(:answer, question: e, value: 'd', answer_sheet_id: app.id)
       expect(e.conditional_match(app)).to be true
     end
     it 'returns true if the conditional_answer is empty and no options are selected' do
@@ -81,6 +81,17 @@ describe Fe::ChoiceField do
       e = create(:choice_field_element, conditional_answer: '', style: 'drop-down')
       qs.pages << create(:page)
       qs.pages.reload.first.elements << e
+      expect(e.conditional_match(app)).to be true
+    end
+    it 'returns true regardless of whitespace separating conditional answers' do
+      qs = create(:question_sheet)
+      app = create(:application)
+      app.question_sheets << qs
+      e = create(:choice_field_element, conditional_answer: "a; b\n", style: 'drop-down')
+      qs.pages << create(:page)
+      qs.pages.reload.first.elements << e
+      a = create(:answer, question: e, value: 'c', answer_sheet_id: app.id)
+      a = create(:answer, question: e, value: 'd', answer_sheet_id: app.id)
       expect(e.conditional_match(app)).to be true
     end
   end
