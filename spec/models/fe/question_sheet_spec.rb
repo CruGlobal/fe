ae36c039dc2da9ce418c15bbf38eb4ab85f6d027
Andrew Roth <andrewroth@gmail.com>
2015-11-10 00:26:21 -0500

    questions_count not needed any more

--- spec/models/fe/question_sheet_spec.rb
+++ spec/models/fe/question_sheet_spec.rb
@@ -6,19 +6,6 @@ describe Fe::QuestionSheet do
   it { expect validate_presence_of :label }
   it { expect validate_uniqueness_of :label }
 
-  context '#questions_count' do
-    it 'should count elements in a grid' do
-      s = create(:question_sheet)
-      p = create(:page, question_sheet: s)
-      e = create(:question_grid)
-      create(:page_element, page: p, element: e)
-      create(:text_field_element, question_grid: e)
-      create(:section, question_grid: e) # this shouldn't get counted
-      p.reload # get the updated all_element_ids column
-      expect(s.questions_count).to eq(1)
-    end
-  end
-
   context '#all_elements' do
     it 'should return elements in the same order as the ids' do
       s = create(:question_sheet)
