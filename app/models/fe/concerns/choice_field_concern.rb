f670dbd5f75c3c4c1b4388c979e536711bf729b6
Andrew Roth <andrewroth@gmail.com>
2016-08-05 21:06:22 -0400

    implement rating text before, after and a dont know option

--- app/models/fe/concerns/choice_field_concern.rb
+++ app/models/fe/concerns/choice_field_concern.rb
@@ -14,8 +14,29 @@ module Fe
     begin
       included do
         has_many :elements, :class_name => "Element", :foreign_key => "choice_field_id", :dependent => :nullify#, :order => :position
+        serialize :rating_before_label_translations, Hash
+        serialize :rating_after_label_translations, Hash
+        serialize :rating_na_label_translations, Hash
       end
     rescue ActiveSupport::Concern::MultipleIncludedBlocks
+    end
+
+    def rating_before_label(locale = nil)
+      rating_before_label_translations &&
+        rating_before_label_translations[locale].present? ?
+        rating_before_label_translations[locale] : self[:rating_before_label]
+    end
+
+    def rating_after_label(locale = nil)
+      rating_after_label_translations && 
+        rating_after_label_translations[locale].present? ?
+        rating_after_label_translations[locale] : self[:rating_after_label]
+    end
+
+    def rating_na_label(locale = nil)
+      rating_na_label_translations && 
+        rating_na_label_translations[locale].present? ?
+        rating_na_label_translations[locale] : self[:rating_na_label]
     end
 
     # Returns choices stored one per line in content field
