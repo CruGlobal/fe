8fda99dd0cdc2174a5fa5fa3498f668139134bb7
Everaldo Gomes <everaldo.gomes@gmail.com>
2018-11-08 18:13:52 -0200

    fixing before_action that were causing errors

--- app/controllers/fe/submit_pages_controller.rb
+++ app/controllers/fe/submit_pages_controller.rb
@@ -1,14 +1,14 @@
 class Fe::SubmitPagesController < ApplicationController
 
   before_action :setup
-  skip_before_action :get_answer_sheet
+  skip_before_action :get_answer_sheet, raise: false
 
   layout nil
-  
+
   def edit
     @next_page = next_custom_page(@application, 'submit_page')
   end
-  
+
   # save any changes on the submit_page (for auto-save, no server-validation)
   def update
     head :ok
@@ -18,5 +18,5 @@ class Fe::SubmitPagesController < ApplicationController
 
   def setup
     @application = @answer_sheet = Fe.answer_sheet_class.constantize.find(params[:application_id]) unless @application
-  end  
+  end
 end
