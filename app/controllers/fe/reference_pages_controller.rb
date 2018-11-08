2b27b34577820320848a5eec87d5655cd132a35d
Everaldo Gomes <everaldo.gomes@gmail.com>
2018-11-08 18:13:52 -0200

    fixing before_action that were causing errors

--- app/controllers/fe/reference_pages_controller.rb
+++ app/controllers/fe/reference_pages_controller.rb
@@ -1,11 +1,11 @@
 # gather reference information from Applicant
 module Fe
   class ReferencePagesController < ApplicationController
-    skip_before_action :cas_filter
+    skip_before_action :cas_filter, raise: false
     skip_before_action :authentication_filter
-    
+
     layout nil
-    
+
     before_action :setup
 
     MONTHS_KNOWN_OPTIONS = [
@@ -15,27 +15,27 @@ module Fe
       ["2 years", 24],
       ["3 or more years", 36]
     ]
-    
+
     # Allow applicant to edit reference
     # /applications/1/reference_page/edit
     # js: provide a partial to replace the answer_sheets page area
     # html?: return a full page for editing reference independantly (after submission)
     def edit
       @references = @application.reference_sheets
-      
+
       # NEXT: skipping all the fancy answer sheets stuff since all custom pages come after those
       @next_page = next_custom_page(@application, 'reference_page')
     end
-    
+
     def update
       #update_references
       head :ok
     end
-    
+
     private
     def setup
       @application = Application.find(params[:application_id])
-    end  
-    
+    end
+
   end
 end
