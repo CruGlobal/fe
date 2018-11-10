6d3e3d72205e8cf94b5cf7e50e5994d83464c9f8
Everaldo Gomes <everaldo.gomes@gmail.com>
2018-11-10 00:00:09 -0200

    Fix - Deprecation Warning - Using Arel.sql with dangerours non-attributes arguments on queries
    DEPRECATION WARNING: Dangerous query method (method whose arguments are used as raw SQL) called with non-attribute argument(s):

--- app/models/fe/question_sheet.rb
+++ app/models/fe/question_sheet.rb
@@ -39,7 +39,7 @@ module Fe
 
     def all_elements
       element_ids = pages.pluck(:all_element_ids).compact.join(',').split(',').find_all(&:present?)
-      element_ids.present? ? Element.where(id: element_ids).order(element_ids.collect{ |id| "id=#{id} DESC" }.join(', ')) : Element.where("1 = 0")
+      element_ids.present? ? Element.where(id: element_ids).order(Arel.sql(element_ids.collect{ |id| "id=#{id} DESC" }.join(', '))) : Element.where("1 = 0")
     end
 
     # Pages get duplicated
